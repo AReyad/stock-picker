e4a9d41b814b5623237ce7d52839c9e6e513a5f5
Reyad <20869625+AReyad@users.noreply.github.com>
2025-09-16 05:40:12 +0300

    Refactor logic implementation

--- stock-picker.rb
+++ stock-picker.rb
@@ -1,37 +1,16 @@
 def stock_picker(array)
-  array_of_profits = []
   best_days = []
-  # gets the heighest profit possible
-  array.each_with_index do |price, day|
-    buy_day = 0
-    sell_day = day
-    while buy_day < sell_day
-      profit = array[sell_day] - array[buy_day] 
-      buy_day += 1
-      if profit > 0
-        array_of_profits.push(profit)
+  profit = 0
+  array.each_with_index do |buy_price, buy_day|
+    array.each_with_index do |sell_price, sell_day|
+      if buy_day < sell_day && (sell_price - buy_price) > profit
+        profit = sell_price - buy_price
+        best_days = [buy_day, sell_day]
       end
     end
   end
-  heighest_profit = array_of_profits.max(1)
-  # gets the days that make such profit
-  array.each_with_index do |price, day|
-    buy_day = 0
-    sell_day = day
-      while buy_day < sell_day
-       profit = array[sell_day] - array[buy_day] 
-       if profit == heighest_profit[0] && !best_days[1]
-        best_days.push(buy_day, sell_day)
-       end
-       buy_day += 1
-      end
-  end
-  if best_days != []
-    best_days
-  else
-    "You cannot make profit out of this stock"
-  end
+  best_days == [] ? "You cannot make profit out of this stock" : best_days
 end
 
-p stock_picker([17,3,6,9,15,8,6,1,10]) # => [1,4]
-p stock_picker([9, 8, 7, 6, 5, 4, 3, 2, 1]) # => No profit
+p stock_picker([17,3,6,9,15,8,6,1,10]) # => [1, 4]
+p stock_picker([9, 8, 7, 6, 5, 4, 3, 2, 1]) # => "You cannot make profit out of this stock"
